9d69f212366ec401756b1eb0775532976ed58be8
paddington22 <pkorolev23@gmail.com>
2024-11-20 18:26:30 +0300

    add db helper

---
+++ core/database/db_helper.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from settings.config import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+
+db_helper = DatabaseHelper(
+    url=settings.get_alchemy_connect_string(),
+    echo=settings.db_echo,
+)
