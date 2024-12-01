016beee6ccaaa27341fc0b22d5b8fe2914735da5
paddington22 <pkorolev23@gmail.com>
2024-12-01 21:18:00 +0300

    add update and delete

--- api/v2024_11_19/products/crud.py
+++ api/v2024_11_19/products/crud.py
@@ -3,7 +3,7 @@ from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import Product
 
-from .schemas import ProductCreate, ProductResponse
+from .schemas import ProductCreate, ProductUpdate, ProductUpdatePartial
 
 
 async def get_products(session: AsyncSession) -> list[Product]:
@@ -23,3 +23,23 @@ async def create_product(session: AsyncSession, payload: ProductCreate) -> Produ
     await session.commit()
     # await session.refresh(product)
     return product
+
+
+async def update_product(
+    session: AsyncSession,
+    product: Product,
+    product_update: ProductUpdate | ProductUpdatePartial,
+    partial: bool = False,
+) -> Product:
+    for name, value in product_update.model_dump(exclude_unset=partial).items():
+        setattr(product, name, value)
+    await session.commit()
+    return product
+
+
+async def delete_product(
+    session: AsyncSession,
+    product: Product,
+) -> None:
+    await session.delete(product)
+    await session.commit()
