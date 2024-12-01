b2e5794a0982a324fc410d0b228ee5bf437a0336
paddington22 <pkorolev23@gmail.com>
2024-12-01 21:17:35 +0300

    add update schemas

--- api/v2024_11_19/products/schemas.py
+++ api/v2024_11_19/products/schemas.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, ConfigDict
 
 class ProductBase(BaseModel):
     title: str
-    description: str | None
+    description: str | None = None
     unit_price: float
 
 
@@ -15,3 +15,12 @@ class ProductResponse(ProductBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+
+
+class ProductUpdate(ProductCreate):
+    pass
+
+
+class ProductUpdatePartial(ProductBase):
+    title: str | None = None
+    unit_price: float | None = None
