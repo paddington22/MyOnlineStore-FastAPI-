b232831e69c4484f6e6d236d948fcfcf76f439a3
paddington22 <pkorolev23@gmail.com>
2024-12-01 20:40:21 +0300

    add products routes

---
+++ api/v2024_11_19/products/routes.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+
+from . import crud
+from .schemas import ProductResponse, ProductCreate
+from core.database import db_helper, Product
+
+router = APIRouter(prefix="/products", tags=["Products"])
+
+
+@router.get("", response_model=list[ProductResponse])
+async def get_products(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):  # -> ProductResponse:
+    return await crud.get_products(session=session)
+
+
+@router.post("", response_model=ProductResponse)
+async def create_product(
+    payload: ProductCreate,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    try:
+        product = await crud.create_product(session=session, payload=payload)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Product {payload.title} already exists",
+        )
+    return product
+
+
+@router.get("/{product_id}", response_model=ProductResponse)
+async def get_product(
+    product_id: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    product = await crud.get_product(session=session, product_id=product_id)
+    if product:
+        return product
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Product {product_id} not found"
+    )
--- core/database/__init__.py
+++ core/database/__init__.py
@@ -3,8 +3,11 @@ __all__ = (
     "DatabaseHelper",
     "db_helper",
     "Users",
+    "Product",
 )
+
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .users import Users
+from .products import Product
